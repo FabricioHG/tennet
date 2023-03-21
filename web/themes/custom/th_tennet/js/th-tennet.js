516663d12daa82e452b3e1d4b56a79a790f8fed3
Rama Fabricio <fabriciofranciscohernandezgome@gmail.com>
2023-03-21 17:44:59 -0600

    Inicio

---
+++ web/themes/custom/th_tennet/js/th-tennet.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * th_tennet behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.thTennet = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
