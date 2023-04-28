3af1064ba2d82551933ba6958b1be23cb0f5e249
Rama Fabricio <fabriciofranciscohernandezgome@gmail.com>
2023-04-28 16:22:02 -0500

    Segunda version para produccion con cambios de Felipe

--- web/themes/custom/th_tennet/js/th-tennet.js
+++ web/themes/custom/th_tennet/js/th-tennet.js
@@ -5,12 +5,15 @@
 (function ($) {
 
   'use strict';
+
   
 
   Drupal.behaviors.thTennet = {
     attach: function (context, settings) {
 
-    
+    $( ".form-item-nombre, .form-item-correo-electronico" ).wrapAll( "<div class='blok_cont_1'></div>" );
+    $( ".form-item-mensaje" ).wrap( "<div class='blok_cont_2'></div>" );
+    $('.form-item-nombre').before("<img src='/sites/default/files/recepcionista_2.png'>");
 
 
 
